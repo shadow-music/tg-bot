7a2ba8653c5114b638049a8571446442a79cc5d2
shadow-music <shadowmusic.og@gmail.com>
2024-11-29 10:26:45 +0000

    02

--- telegram-bot-project/bot.js
+++ telegram-bot-project/bot.js
@@ -59,3 +59,80 @@ supportHandler(bot, ADMIN_ID);
 
 // ارسال خودکار پیام‌ها
 scheduleMessage(bot, require('./userController').User);
+
+
+
+
+
+
+
+
+// const TelegramBot = require('node-telegram-bot-api');
+// const connectDB = require('./db');
+// const { addFile, getFiles, getFileByCode } = require('./fileController');
+// const crypto = require('crypto');
+
+// // تنظیمات
+// const TOKEN = '7835327718:AAHcPn1qw60RIdv-ai5xFHde6c-EjHaGBjM';
+// const ADMIN_ID = 6087657605; // آیدی عددی مدیر
+// const BOT_USERNAME = 'SoundCloudmusic_bot'; // نام کاربری ربات
+// const bot = new TelegramBot(TOKEN, { polling: true });
+
+// // اتصال به دیتابیس
+// connectDB();
+
+// // متغیر برای پیگیری وضعیت مدیر
+// let waitingForFile = false;
+
+// // هندلر برای دستور /addfile
+// bot.onText(/\/addfile/, (msg) => {
+//   const chatId = msg.chat.id;
+
+//   // بررسی اینکه آیا کاربر مدیر است
+//   if (chatId === ADMIN_ID) {
+//     waitingForFile = true; // ربات منتظر فایل خواهد بود
+//     bot.sendMessage(chatId, '✅ لطفاً فایل خود را ارسال کنید.');
+//   }
+// });
+
+// // هندلر برای دریافت فایل
+// bot.on('document', async (msg) => {
+//   const chatId = msg.chat.id;
+
+//   // بررسی اینکه فایل از طرف مدیر ارسال شده است
+//   if (chatId === ADMIN_ID && waitingForFile) {
+//     const fileId = msg.document.file_id; // گرفتن file_id
+//     const fileName = msg.document.file_name; // گرفتن نام فایل
+
+//     // تولید یک کد یکتا برای فایل
+//     const uniqueCode = crypto.randomBytes(10).toString('hex');
+
+//     // ذخیره فایل در دیتابیس
+//     await addFile(fileName, fileId, uniqueCode);
+
+//     // ایجاد لینک استارت برای فایل
+//     const fileLink = `https://t.me/${BOT_USERNAME}?start=${uniqueCode}`;
+
+//     // ارسال لینک به مدیر
+//     bot.sendMessage(chatId, `✅ فایل با موفقیت ذخیره شد. لینک زیر را می‌توانید برای دسترسی به فایل به اشتراک بگذارید:\n${fileLink}`);
+
+//     waitingForFile = false; // حالت انتظار فایل را غیرفعال کنید
+//   }
+// });
+
+// // هندلر برای دستور /start با کد یکتا
+// bot.onText(/\/start (.+)/, async (msg, match) => {
+//   const chatId = msg.chat.id;
+//   const uniqueCode = match[1]; // گرفتن کد یکتا
+
+//   // پیدا کردن فایل مربوط به کد یکتا
+//   const file = await getFileByCode(uniqueCode);
+
+//   if (file) {
+//     // ارسال فایل به کاربر
+//     bot.sendDocument(chatId, file.fileId, { caption: `📁 ${file.fileName}` });
+//   } else {
+//     // اگر کد معتبر نبود
+//     bot.sendMessage(chatId, '❌ فایل مورد نظر یافت نشد.');
+//   }
+// });
--- telegram-bot-project/fileController.js
+++ telegram-bot-project/fileController.js
@@ -18,3 +18,41 @@ const getFiles = async () => {
 };
 
 module.exports = { addFile, getFiles };
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// // تعریف مدل فایل
+// const fileSchema = new mongoose.Schema({
+//   fileName: { type: String, required: true },
+//   fileId: { type: String, required: true },
+//   uniqueCode: { type: String, required: true, unique: true }, // کد یکتا
+// });
+
+// const File = mongoose.model('File', fileSchema);
+
+// // افزودن فایل جدید
+// const addFile = async (fileName, fileId, uniqueCode) => {
+//   const newFile = new File({ fileName, fileId, uniqueCode });
+//   await newFile.save();
+//   console.log('✅ File added:', fileName);
+// };
+
+// // دریافت فایل‌ها
+// const getFiles = async () => {
+//   return await File.find();
+// };
+
+// // پیدا کردن فایل بر اساس کد یکتا
+// const getFileByCode = async (uniqueCode) => {
+//   return await File.findOne({ uniqueCode });
+// };
+
+// module.exports = { addFile, getFiles, getFileByCode };
